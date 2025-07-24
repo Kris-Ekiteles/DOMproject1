9fe9e01cc3a99a032e8d27c4254e26cebb442210
kris <krisekteles@gmail.com>
2025-07-24 19:26:06 +0300

    changes to the script file to update counter

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
-const increaseBtn = document.querySelectorAll(".fa-plus-circle");
-const decreaseBtn = document.querySelectorAll(".decrease-btn");
-const spaceBtn = document.querySelectorAll(".quantity");
-//sumPrice
-const PriceSum = document.getElementById("total-sum");
-const priceTag = document.getElementById("unit-price");
+document.addEventListener("DOMContentLoaded", function () {
+  const increaseBtn = document.querySelector("#increase-btn");
+  const decreaseBtn = document.querySelector("#decrease-btn");
+  const spaceBtn = document.querySelector("#display-space");
+  const PriceSum = document.getElementById("total");
+  const priceTag = document.getElementById("price");
 
-let count = 0;
-const minCount = 1;
-const maxCount = 10;
-// total price
+  let count = 0;
+  let minCount = 0;
+  let maxCount = 10;
 
-function counterUpdate() {
-  spaceBtn.textContent = count;
-  decreaseBtn.disabled = count <= minCount;
-  increaseBtn.disabled = count >= maxCount;
-}
-decreaseBtn.addEventListener("click", () => {
-  if (count > minCount) {
-    count--;
-    counterUpdate();
+  function counterUpdate() {
+    spaceBtn.textContent = count;
+    decreaseBtn.disabled = count <= minCount;
+    increaseBtn.disabled = count >= maxCount;
   }
+  // decrease button.
+  decreaseBtn.addEventListener("click", () => {
+    if (count > minCount) {
+      count--;
+      counterUpdate();
+    }
+  });
+  //increase button
+  increaseBtn.addEventListener("click", () => {
+    if (count < maxCount) {
+      count++;
+      counterUpdate();
+    }
+  });
+  counterUpdate();
 });
-increaseBtn.addEventListener("click", () => {
-  if (count <= maxCount) {
-    count++;
-    counterUpdate();
-  }
-});
-counterUpdate();
